f8e91a233cfee6c3af69492bded8b0291021322b
BrazAc <braz0502002@gmail.com>
2026-06-26 21:57:00 -0300

    Incluindo a janela principal e dashboard. Criado o metodo orgaizarWidgetJanelaPrincipal que adiciona as views do programa ao baralho (QStackWidget) da janela principal e conecta os sinais disparados pelos botoes para transicionar entre as views

--- src/controller/Controller.py
+++ src/controller/Controller.py
@@ -1,9 +1,10 @@
 from PySide6.QtWidgets import QApplication
 # Importar model
-# Importar views
-from src.view.JanelaLogin import JanelaLogin
 from src.model.Model import Model
-
+# Importar views
+from src.view.JanelaPrincipal import JanelaPrincipal
+from src.view.JanelaLogin import JanelaLogin
+from src.view.JanelaDashboard import JanelaDashboard
 
 class Controller:
     def __init__(self, app):
@@ -12,7 +13,29 @@ class Controller:
         self.__model = Model(Controller=self)
         
         # Views
+        self.__viewJanelaPrincipal = JanelaPrincipal(self.__app)
         self.__viewJanelaLogin = JanelaLogin(self.__app)
-        
+        self.__viewJanelaDashboard = JanelaDashboard(self.__app)
+
+        # Organizar Widget de baralho de janelas
+        self.organizarWidgetStackJanelaPrincipal()
+
+    def organizarWidgetStackJanelaPrincipal(self):
+        self.__viewJanelaPrincipal
+
+        # Adicionar views ao baralho da janela principal
+        self.__viewJanelaPrincipal.widgetStackJanelas.addWidget(self.__viewJanelaLogin)     # Índice 0
+        self.__viewJanelaPrincipal.widgetStackJanelas.addWidget(self.__viewJanelaDashboard) # Índice 1
+
+        # Conectar sinais
+        self.__viewJanelaLogin.sinal_btnGerenteApertado.connect(self.mudarView_dashboard)
+
+    # Mudar views
+    def mudarView_login(self):
+        self.__viewJanelaPrincipal.widgetStackJanelas.setCurrentIndex(0)
+    def mudarView_dashboard(self):
+        self.__viewJanelaPrincipal.widgetStackJanelas.setCurrentIndex(1)
+
     def iniciarView(self):
-        self.__viewJanelaLogin.mostrarJanela()
+        self.mudarView_login()
+        self.__viewJanelaPrincipal.mostrarJanela()
