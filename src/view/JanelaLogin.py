03b30b5971e22cc2396f0a41d42fb2a725ebf7e7
BrazAc <braz0502002@gmail.com>
2026-06-27 09:45:03 -0300

    Alterada estrutura da janela para formulário de login ficar a direita e imagem a esquerda

--- src/view/JanelaLogin.py
+++ src/view/JanelaLogin.py
@@ -12,8 +12,12 @@ class JanelaLogin(QWidget):
         self.layout_principal = None
         self.layout_frame_labels = None
         self.layout_frame_botoes = None
+        self.layout_frame_IMAGEM = None
+        self.layout_frame_LOGIN = None
         self.frame_labels = None
         self.frame_botoes = None
+        self.frame_IMAGEM = None
+        self.frame_LOGIN = None
         self.label_textoBoasVindas = None
         self.botao_gerente = None
         self.botao_operador = None
@@ -26,13 +30,17 @@ class JanelaLogin(QWidget):
 
     def __criarWidgets(self):
         # Layouts
-        self.layout_principal = QVBoxLayout(self)
+        self.layout_principal = QHBoxLayout(self)
         self.layout_frame_labels = QHBoxLayout()
         self.layout_frame_botoes = QHBoxLayout()
+        self.layout_frame_IMAGEM = QHBoxLayout()
+        self.layout_frame_LOGIN = QVBoxLayout()
 
         # Frames
         self.frame_labels = QFrame() 
         self.frame_botoes = QFrame()
+        self.frame_IMAGEM = QFrame()
+        self.frame_LOGIN = QFrame()
 
         # Labels
         self.label_textoBoasVindas = QLabel("Boas vindas. Faça login:")
@@ -48,6 +56,20 @@ class JanelaLogin(QWidget):
         # Botao operador
         self.botao_operador.clicked.connect(self.sinal_btnOperadorApertado.emit)
 
+        # Frame imagem
+        self.frame_IMAGEM.setFrameShape(QFrame.StyledPanel) 
+        self.frame_IMAGEM.setStyleSheet("""
+                                        QLabel {
+                                            background-color: white;
+                                            color: black;
+                                            border: none;
+                                            border-radius: 5px;
+                                            padding: 10px;
+                                        }""")
+        self.layout_frame_IMAGEM.addWidget(self.label_textoBoasVindas) 
+        self.frame_IMAGEM.setLayout(self.layout_frame_IMAGEM)
+        self.layout_principal.addWidget(self.frame_IMAGEM)   # Adicionar frame ao layout principal
+
         # Frame labels
         self.frame_labels.setFrameShape(QFrame.StyledPanel) 
         self.frame_labels.setStyleSheet("""
@@ -60,7 +82,7 @@ class JanelaLogin(QWidget):
                                         }""")
         self.layout_frame_labels.addWidget(self.label_textoBoasVindas) 
         self.frame_labels.setLayout(self.layout_frame_labels)
-        self.layout_principal.addWidget(self.frame_labels)   # Adicionar frame ao layout principal
+        self.layout_frame_LOGIN.addWidget(self.frame_labels)   # Adicionar frame ao layout de login
 
         # Frame botoes
         self.frame_botoes.setFrameShape(QFrame.StyledPanel) 
@@ -75,7 +97,10 @@ class JanelaLogin(QWidget):
         self.layout_frame_botoes.addWidget(self.botao_gerente)
         self.layout_frame_botoes.addWidget(self.botao_operador)
         self.frame_botoes.setLayout(self.layout_frame_botoes)
-        self.layout_principal.addWidget(self.frame_botoes)   # Adicionar frame ao layout principal
+        self.layout_frame_LOGIN.addWidget(self.frame_botoes)   # Adicionar frame ao layout de login
+
+        self.frame_LOGIN.setLayout(self.layout_frame_LOGIN)   # Adicionar layout_frame_login ao frame login
+        self.layout_principal.addWidget(self.frame_LOGIN)    # Adicionar frame login ao layout principal
 
         # Atribuir layout_principal a janela da classe
         self.setLayout(self.layout_principal)
