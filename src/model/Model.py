bda2d37d8d77b8ee324e46388ce073afa8eb0e9b
BrazAc <braz0502002@gmail.com>
2026-06-29 13:48:39 -0300

    Adicionado método para consultar todas as viagens cadastradas e retornar para o controller

--- src/model/Model.py
+++ src/model/Model.py
@@ -1,3 +1,8 @@
 class Model:
     def __init__(self, Controller):
         self.__controller = Controller
+
+    def consultarViagens(self):
+        # Aqui você vai fazer o SELECT SUA MÃE FROM TABELA
+        listaViagens = ['1,2,3,4', '1,2,3,4', '1,2,3,4']
+        return listaViagens
